8982266858a326903ba1d8607ebf1a0f7f36b1b3
EudiaApentsui <apentsuieudia@gmail.com>
2023-02-23 01:46:46 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - a function that adds two integers and returns the result
+ *
+ * @a: para 1
+ * @b: para 2
+ *
+ * Return: add
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
